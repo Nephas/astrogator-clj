8ecac335230fbd5a6af0691dca199cb3e1c8a4fa
Marco Fink <marco.fink@zoho.eu>
2019-03-07 21:54:24 +0100

    move clojure.png * add min-lein-version

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject astrogator_clj "0.1.0-SNAPSHOT"
   :description "Spaaaaace..."
   :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.520"]
 
