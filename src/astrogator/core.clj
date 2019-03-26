0ac377f37a59d7d0253d65f80db256f6f95dafad
Marco Fink <marco.fink@zoho.eu>
2019-03-27 00:36:00 +0100

    use ubuntu font on browser

--- src/astrogator/core.clj
+++ src/astrogator/core.clj
@@ -19,7 +19,7 @@
 (defn setup []
   (do (reset! screen (q/current-graphics))
       (q/frame-rate c/frame-rate)
-      (q/text-font (q/create-font "src/data/conthrax-sb.ttf" 14 true))
+      (q/text-font (q/create-font "src/data/conthrax.ttf" 14 true))
       (q/color-mode :hsb 1.0 1.0 1.0 255)
       (q/ellipse-mode :radius)
       (q/no-stroke)
